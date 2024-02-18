adf5cfd0245bc3b23ab4a82092a7d0a0e5c9d68d
mahi000007 <mahendran1482004@gmail.com>
2024-02-18 19:15:13 +0530

    feat: learn python template engine

--- helpingbuddy/base/views.py
+++ helpingbuddy/base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1, 'name': 'Python Buddy'},
+    {'id':2, 'name': 'Java Buddy'},
+    {'id':3, 'name': 'Dart Buddy'}
+]
 
 def home(request):
-    return render(request, 'home.html');
+    return render(request, 'home.html', {'rooms':rooms});
 
 def room(request):
     return render(request, 'room.html');
