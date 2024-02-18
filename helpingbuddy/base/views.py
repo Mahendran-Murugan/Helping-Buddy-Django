309eca4f51133ecaac1a7346db9e5db76bd428f1
mahi000007 <mahendran1482004@gmail.com>
2024-02-18 19:33:00 +0530

    feat: learn urls and view and names

--- helpingbuddy/base/views.py
+++ helpingbuddy/base/views.py
@@ -10,5 +10,10 @@ def home(request):
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'base/room.html');
+def room(request, s):
+    room = None
+    for i in rooms:
+        if i['id'] == int(s):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context);
