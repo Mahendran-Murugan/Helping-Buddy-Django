8de453c337f4c882fe1ade3a4f00c074a87ba222
mahi000007 <mahendran1482004@gmail.com>
2024-02-18 18:35:15 +0530

    feat: relocate env and create app

--- helpingbuddy/env/bin/activate.fish
+++ helpingbuddy/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mahendran/Documents/Django-Projects/env"
+set -gx VIRTUAL_ENV "/Users/mahendran/Documents/Django-Projects/helpingbuddy/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
