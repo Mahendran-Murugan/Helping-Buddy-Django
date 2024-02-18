e4e148b32a81283aadab5680b3d103749da1acd7
mahi000007 <mahendran1482004@gmail.com>
2024-02-18 18:57:50 +0530

    feat: add template folder and learn template iheritance

--- helpingbuddy/helpingbuddy/settings.py
+++ helpingbuddy/helpingbuddy/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.app.BaseConfig'
+    'base.apps.BaseConfig'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'helpingbuddy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'    
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
