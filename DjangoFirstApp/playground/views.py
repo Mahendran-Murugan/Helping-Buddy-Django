5ad7a9c4900cd5f0ab78c199bb6ffca4eaaced24
mahi000007 <mahendran1482004@gmail.com>
2024-02-18 17:11:48 +0530

    fix: remove old unused files

--- DjangoFirstApp/playground/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-# Create your views here.
-# request handler
-
-def sayHello(request):
-    return render(request, 'hello.html')
--- socialMediaApp/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse('<h1> Welcom to Social Media<h1>')
