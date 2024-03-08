fc9aea04001a08cd8d07ff75e87b923b26a89149
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-08 18:17:37 +0530

    feat: add search functionality

--- helpingbuddy/base/views.py
+++ helpingbuddy/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # rooms = [
@@ -9,8 +9,10 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topic = Topic.objects.all()
+    context = {'rooms':rooms,"topics":topic}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
