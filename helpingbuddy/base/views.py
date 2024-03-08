ee90fa60bbc68dc05c34fc2dff1a7cdb7a83ec2b
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-08 08:57:21 +0530

    feat: connect view with local database

--- helpingbuddy/base/views.py
+++ helpingbuddy/base/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id':1, 'name': 'Python Buddy'},
-    {'id':2, 'name': 'Java Buddy'},
-    {'id':3, 'name': 'Dart Buddy'}
-]
+# rooms = [
+#     {'id':1, 'name': 'Python Buddy'},
+#     {'id':2, 'name': 'Java Buddy'},
+#     {'id':3, 'name': 'Dart Buddy'}
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, s):
-    room = None
-    for i in rooms:
-        if i['id'] == int(s):
-            room = i
+    room = Room.objects.get(id=s)
     context = {'room': room}
     return render(request, 'base/room.html', context)
