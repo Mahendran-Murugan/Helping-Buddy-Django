69c24061087efd8fefc5796b3241be16e534c7f3
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-08 08:24:42 +0530

    feat: apply migrations

--- helpingbuddy/base/views.py
+++ helpingbuddy/base/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 
-rooms = [
-    {'id':1, 'name': 'Python Buddy'},
-    {'id':2, 'name': 'Java Buddy'},
-    {'id':3, 'name': 'Dart Buddy'}
-]
-
 def home(request):
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
