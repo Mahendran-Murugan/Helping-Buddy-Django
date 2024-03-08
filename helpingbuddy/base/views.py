8562172fec31a9d97dd071b55f593e04c0d0a4d9
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-08 18:32:14 +0530

    feat: add global search functionality

--- helpingbuddy/base/views.py
+++ helpingbuddy/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -10,9 +11,10 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains = q))
+    roomCount = rooms.count()
     topic = Topic.objects.all()
-    context = {'rooms':rooms,"topics":topic}
+    context = {'rooms':rooms,"topics":topic,'room_count':roomCount}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
