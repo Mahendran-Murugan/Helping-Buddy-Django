19bd31913d732624e97153030dbd43c53222407b
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-08 09:10:31 +0530

    feat: make migrations

--- helpingbuddy/base/models.py
+++ helpingbuddy/base/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import SET_NULL
 
-# Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
     # host = 
-    # topic = 
+    topic = models.ForeignKey(Topic, on_delete = models.SET_NULL, null = True)
     name = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank = True)
     # participants =
@@ -13,3 +19,16 @@ class Room(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+    room = models.ForeignKey(Room, on_delete = models.SET_NULL, null = True)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now = True) #take snapshot of time when modified
+    created = models.DateTimeField(auto_now_add = True) #take snapshot of time when created
+    
+    
+    def __str__(self):
+        return self.body[0:50]
+    
