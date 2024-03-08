63fcbc9e8e209ab9f99a9d464c20b22f7d8837c7
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-08 08:40:29 +0530

    feat: add model

--- helpingbuddy/base/models.py
+++ helpingbuddy/base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length = 200)
+    description = models.TextField(null = True, blank = True)
+    # participants =
+    updated = models.DateTimeField(auto_now = True) #take snapshot of time when modified
+    created = models.DateTimeField(auto_now_add = True) #take snapshot of time when created
+    
+    def __str__(self):
+        return self.name
