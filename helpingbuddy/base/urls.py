00ee6dd52709605f3ca1f2cefbf67f3eea30ff14
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-08 09:34:05 +0530

    feat: add room form

--- helpingbuddy/base/urls.py
+++ helpingbuddy/base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:s>/', views.room, name = 'room'),
+    path('create-room/', views.createRoom, name= 'create-room')
 ]
--- helpingbuddy/base/views.py
+++ helpingbuddy/base/views.py
@@ -16,3 +16,7 @@ def room(request, s):
     room = Room.objects.get(id=s)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
