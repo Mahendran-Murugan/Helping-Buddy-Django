9c90be74cc7ce3015fc86215499b410e59afa6c7
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-08 09:11:55 +0530

    feat: add Models in admin

--- helpingbuddy/base/models.py
+++ helpingbuddy/base/models.py
@@ -9,7 +9,7 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    # host = 
+    host = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
     topic = models.ForeignKey(Topic, on_delete = models.SET_NULL, null = True)
     name = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank = True)
