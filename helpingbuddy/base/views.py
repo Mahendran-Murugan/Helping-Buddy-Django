7bd9b84baa7066c0f3129e02d161fc1eac7f976e
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-09 14:56:24 +0530

    feat: add room comments and description

--- helpingbuddy/base/views.py
+++ helpingbuddy/base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # rooms = [
@@ -73,7 +73,16 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    comments = room.message_set.all().order_by('-created')
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room',pk = room.id)
+    
+    context = {'room': room, 'comments':comments}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
